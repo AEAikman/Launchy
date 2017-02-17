6360792a658e6c012b11972e8738f47517f86680
AEAikman <aaikman@halon.com>
2017-02-17 11:52:15 -0800

    added noteTaker

---
+++ Batch/AddToNotes.bat
@@ -0,0 +1,24 @@
+rem AddToNotes.bat 
+rem Aaron Aikman
+
+rem For quickly adding notes to a text file in the designated path
+rem The first word entered will indicate which file to write to
+rem If the file does not exist, it will be created
+
+@ECHO OFF
+
+call :subr %*
+exit /b
+
+:subr
+
+set numberNewlines=2
+set notePath=Z:\InProd\Ozzy\artists\Aaron\Text\Notes\Notes_%1.txt
+
+for /L %%i IN (1,1,%numberNewlines%) do (ECHO. >> %notePath%)
+
+for %%A in (%*) do (
+	IF NOT %%A==%1 <NUL set /p= %%A >> %notePath%
+)
+
+exit /b
