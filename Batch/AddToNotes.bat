74d36717bfe1725d4912914fc499186cfe6e4be2
AEAikman <aaikman@halon.com>
2017-02-17 17:42:51 -0800

    AddToNotes update

--- Batch/AddToNotes.bat
+++ Batch/AddToNotes.bat
@@ -4,6 +4,7 @@
 
 :: For quickly adding notes to a text file in the designated path using Launchy
 :: The first word entered will indicate which file to write to
+:: If you want the text file to open upon completion, type open for the second word
 :: If the file does not exist, it will be created
 :: Many symbols do not work
 :: Some symbols that do work include . # ! / \
@@ -20,8 +21,17 @@ set notePath=Z:\InProd\Ozzy\artists\Aaron\Text\Notes\Notes_%1.txt
 
 for /L %%i IN (1,1,%numberNewlines%) do (ECHO. >> %notePath%)
 
-for %%A in (%*) do (
-	IF NOT %%A==%1 <NUL set /p= %%A >> %notePath%
-)
+if not %2==open (for %%A in (%*) do (IF NOT %%A==%1 (<NUL set /p= %%A >> %notePath%)))
+
+if %2==open (
+	for %%A in (%*) do (
+		IF NOT %%A==%1 (
+			IF NOT %%A==%2 (
+				<NUL set /p= %%A >> %notePath%
+				)
+			)
+	)
+	start %notePath%
+) 
 
 exit /b
